07907cf89f1b7b22614cf9538686f768912cb226
Kirill <kkpetrov.dev@gmail.com>
2024-11-07 16:14:56 +0200

    chore(web): initialize `tailwind` & `shadcn`

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,16 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-    return <>Hello World!</>;
+    return (
+        <>
+            <div className="p-5 bg-sky-500 flex items-center justify-center">
+                <span className="bg-red-500 text-white font-black text-5xl p-3 rounded-lg">
+                    Hello World!
+                </span>
+            </div>
+            <Button>shadcn test</Button>
+        </>
+    );
 }
 
 export default App;
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
+import tsConfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react()],
-});
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+    plugins: [react(), tsConfigPaths()],
+    clearScreen: false,
+    server: {
+        port: 3000,
+        strictPort: true,
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+}));
